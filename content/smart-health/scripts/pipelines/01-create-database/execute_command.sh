e1004240d3ccb024d6252bb1337ac66171fa249d
RomuloR0zo <jrozo847@gmail.com>
2025-10-27 08:05:56 -0500

    docs(data): data directory update

--- content/smart-health/scripts/pipelines/01-create-database/execute_command.sh
+++ content/smart-health/scripts/pipelines/01-create-database/execute_command.sh
@@ -1,4 +1,4 @@
 # Primer Script
 python .\01-create-database\01-sql-ddl-script-auto.py --sql-dir ../ddl --user postgres --password "queso" --host localhost --port 5432 --database postgres --create-script true
 # Segundo Script
-python .\01-create-database\01-sql-ddl-script-auto.py --sql-dir ../ddl --user sm_admin --password "****" --host localhost --port 5433 --database smarthdb
+python .\01-create-database\01-sql-ddl-script-auto.py --sql-dir ../ddl --user sm_admin --password "sm2025**" --host localhost --port 5432 --database smarthdb
